460eb05d14cb251b1b3be260963a69a12ca6db48
mariasazhina <msazhina23@gmail.com>
2024-02-25 23:02:05 +0200

    sql queries

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(50)
+);
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	title varchar(50),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(50)
+);
+
